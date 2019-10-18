e62d1804220c842772cad32215c3eadaaa1c7c79
Generalnormak <generalnormak@byui.edu>
2019-10-18 15:23:02 -0500

    basic ground work for website complete.

---
+++ lesson-5/js/responsivebanner-5.js
@@ -0,0 +1,13 @@
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+if dayName = daynames[d.Friday()];
+.pancakes.display = "block"
