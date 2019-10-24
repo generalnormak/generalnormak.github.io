c0976e89ed8a961d810a882b8c19018693c61b41
Generalnormak <generalnormak@byui.edu>
2019-10-23 21:24:57 -0500

    tried changing it to block element

--- lesson-5/js/responsivebanner-5.js
+++ lesson-5/js/responsivebanner-5.js
@@ -4,7 +4,7 @@ function epicpancake(){
 	// console.log(day);
 	//  console.log(date);
 	 if(day == 3) {
-		document.getElementsByClassName("pancakes").style.visibility ="visible";
+		document.getElementsByClassName("pancakes").style.display ="block";
 	 }
 	// }else{
 	// 	document.getElementsByClassName("pancakes").style.display ="none";
