dcf9f2774fd7fb00c62afd925ecbf2071d278a19
Generalnormak <generalnormak@byui.edu>
2019-10-23 19:15:03 -0500

    created skeleton for responsive images lesson

---
+++ lesson-6/js/responsivebanner-6.js
@@ -0,0 +1,14 @@
+function epicpancake(){
+	const date =  new Date();
+	const day = date.getDay();
+	// console.log(day);
+	//  console.log(date);
+	 if(day == 5) {
+		document.getElementsByClassName("pancakes").style.visibility ="hidden";
+	 }
+	// }else{
+	// 	document.getElementsByClassName("pancakes").style.display ="none";
+	// }
+	
+}
+
