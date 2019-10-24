aeb8aa96dbdcccc0d3172477cfd8b30cc0d12adf
Generalnormak <generalnormak@byui.edu>
2019-10-23 21:53:53 -0500

    figured it out finally!

--- lesson-5/js/responsivebanner-5.js
+++ lesson-5/js/responsivebanner-5.js
@@ -1,14 +1,10 @@
-function epicpancake(){
-	const date =  new Date();
-	const day = date.getDay();
-	// console.log(day);
-	//  console.log(date);
-	 if(day == 3) {
-		document.getElementsByClassName("pancakes").style.display ="block";
-	 }
-	// }else{
-	// 	document.getElementsByClassName("pancakes").style.display ="none";
-	// }
-	
+const date = new Date();
+const day = date.getDay();
+// console.log(day);
+//  console.log(date);
+if (day == 5) {
+	document.getElementsByClassName("pancakes")[0].style.display = "block";
 }
-
+// }else{
+// 	document.getElementsByClassName("pancakes").style.display ="none";
+// }
