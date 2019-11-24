a430205d890f5a3ab08ff8f9a620ca9aa56af2eb
Generalnormak <generalnormak@byui.edu>
2019-11-23 22:43:50 -0600

    more fixes but not sure if I am getting anywhere

--- lesson-10/js/weatherapiprestonforecast.js
+++ lesson-10/js/weatherapiprestonforecast.js
@@ -2,43 +2,50 @@ const forecastapiURL = "http://api.openweathermap.org/data/2.5/forecast?id=56044
 fetch(forecastapiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-            console.log(jsObject);
-                for (let i = 0; i < jsObject.list.length; i++) {
-                    if (jsObject.list[i].dt_txt.includes('18:00:00')) {
-                        console.log(jsObject.list[i].dt_txt);
-                        let card = document.createElement('section');
-                        let table = document.createElement('table');
-                        let thead = document.createElement('thead');
-                        let caption = document.createElement('caption');
-                        let tbody = document.createElement('tbody');
-                        let tr = document.createElement('tr');
-                        let th = document.createElement('th');
-                        let td = document.createElement('td');
-            
-                        caption.textContent = '5 Day Forcast';
+        console.log(jsObject);
+        for (let i = 0; i < jsObject.list.length; i++) {
+            if (jsObject.list[i].dt_txt.includes('18:00:00')) {
+                console.log(jsObject.list[i].dt_txt);
+                let card = document.createElement('section');
+                let table = document.createElement('table');
+                let thead = document.createElement('thead');
+                let caption = document.createElement('caption');
+                let tbody = document.createElement('tbody');
+                let tr = document.createElement('tr');
+                let th = document.createElement('th');
+                let td = document.createElement('td');
+                let image = document.createElement('img');
 
-                        th.setAttribute = jsObject.list[i].clouds.dt_txt;
+                caption.textContent = '5 Day Forcast';
 
-                        td.setAttribute = jsObject.list[i].weather[0].icon + jsObject.list[i].main.temp + ' &#176;F';
+                th.setAttribute = jsObject.list[i].clouds.dt_txt;
 
-                        card.appendChild(table);
+                'https://openweathermap.org/img/w/' + image.setAttribute('src', jsObject.list[i].weather[0].icon);
 
-                        card.appendChild(thead);
+                td.setAttribute = jsObject.list[i].main.temp + ' &#176;F';
 
-                        card.appendChild(caption);
-                        
-                        card.appendChild(tbody);
+                card.appendChild(table);
 
-                        card.appendChild(tr);
-            
-                        card.appendChild(th);
+                card.appendChild(thead);
 
-                        card.appendChild(tr);
-            
-                        card.appendChild(td);
-            
-                        document.querySelector('div.cards').appendChild(card);
-                    }
-                }
+                card.appendChild(caption);
 
-            });
+                card.appendChild(tbody);
+
+                card.appendChild(tr);
+
+                card.appendChild(th);
+
+                card.appendChild(tr);
+
+                card.appendChild(image);
+
+                card.appendChild(tr);
+
+                card.appendChild(td);
+
+                document.querySelector('div.cards').appendChild(card);
+            }
+        }
+
+    });
