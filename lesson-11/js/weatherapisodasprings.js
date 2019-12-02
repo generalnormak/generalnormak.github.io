858c1d8c98ae2e9014e17d8e0155e9cf78435984
Generalnormak <generalnormak@byui.edu>
2019-12-02 12:51:57 -0600

    found the ids for soda springs and fish haven

---
+++ lesson-11/js/weatherapisodasprings.js
@@ -0,0 +1,10 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Soda Springs,83276&units=imperial&APPID=f7e1535536fec9ae94ea8cdcf72b6ab2";
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        document.getElementById('temp').textContent = jsObject.main.temp;
+        document.getElementById('speed').textContent = jsObject.wind.speed;
+        document.getElementById('humidity').textContent = jsObject.main.humidity;
+        document.getElementById('currently').textContent = jsObject.weather[0].main;
+    });
